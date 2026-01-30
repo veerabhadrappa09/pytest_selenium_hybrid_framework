b0b033bab0b210704c845935ea4619754ed1d2a0
veerabhadrappa09 <55814707+veerabhadrappa09@users.noreply.github.com>
2026-01-30 23:42:53 +0530

    Refactor variable name for loaded config data

--- utils/config.py
+++ utils/config.py
@@ -10,12 +10,14 @@ if not os.path.exists(env_file_path):
     raise FileNotFoundError(f"Environment file not found: {env_file_path}")
 
 with open(env_file_path) as f:
-    data = json.load(f)
+    config = json.load(f)
 
-BASE_URL = data["base_url"]
-USERNAME = data["username"]
-PASSWORD = data["password"]
+BASE_URL = config["base_url"]
+USERNAME = config["username"]
+PASSWORD = config["password"]
+
 
 IMPLICIT_WAIT = 5
 EXPLICIT_WAIT = 10
+
 
